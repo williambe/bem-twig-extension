4693f9c36c444720d33d4d27309e748e7a62488a
williambe <william@willbe.it>
2018-10-08 18:04:38 +0200

    Implement BEM mixes without losing original block name itself

--- bem.function.php
+++ bem.function.php
@@ -9,28 +9,19 @@ use Drupal\Core\Template\Attribute;
 $function = new Twig_SimpleFunction('bem', function ($context, $base_class, $modifiers = array(), $blockname = '', $extra = array()) {
   $classes = [];
 
-  // If using a blockname to override default class.
-  if ($blockname) {
-    // Set blockname class.
-    $classes[] = $blockname . '__' . $base_class;
-
-    // Set blockname--modifier classes for each modifier.
-    if (isset($modifiers) && is_array($modifiers)) {
-      foreach ($modifiers as $modifier) {
-        $classes[] = $blockname . '__' . $base_class . '--' . $modifier;
-      };
-    }
+  // Set base class.
+  $classes[] = $base_class;
+  // Set base--modifier class for each modifier.
+  if (isset($modifiers) && is_array($modifiers)) {
+    foreach ($modifiers as $modifier) {
+      $classes[] = $base_class . '--' . $modifier;
+    };
   }
-  // If not overriding base class.
-  else {
-    // Set base class.
-    $classes[] = $base_class;
-    // Set base--modifier class for each modifier.
-    if (isset($modifiers) && is_array($modifiers)) {
-      foreach ($modifiers as $modifier) {
-        $classes[] = $base_class . '--' . $modifier;
-      };
-    }
+  
+  // If a blockname is provided create a mixes class.
+  if ($blockname) {
+    // Set mixes class.
+    $classes[] = $blockname . '__' . $base_class;
   }
 
   // If extra non-BEM classes are added.
